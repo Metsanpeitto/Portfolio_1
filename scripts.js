44be91e0a98da13376628f6cdf8a42382c388b50
Metsanpeitto <abraham@waldenberginc.com>
2021-06-21 17:25:08 +0200

    Fix linter errors

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
-function handleClick() {
-  let buttons = document.getElementsByClassName('menu-buttons')
-  let menu = document.getElementsByClassName('menu')
-  let main = document.getElementsByTagName('main')
-  let close = document.getElementsByClassName('menu-close')
-  
-  console.log(close)
-  if (buttons[0].style.display == 'none') {
-  buttons[0].style.display = 'grid'
-  main[0].style.filter = 'blur(4px)'
-  close[0].style.display = 'block'
-  menu[0].style.display = 'none'
+const handleClick = () => {
+  const buttons = document.getElementsByClassName('menu-buttons');
+  const menu = document.getElementsByClassName('menu');
+  const main = document.getElementsByTagName('main');
+  const close = document.getElementsByClassName('menu-close');
+
+  if (buttons[0].style.display === 'none') {
+    buttons[0].style.display = 'grid';
+    main[0].style.filter = 'blur(4px)';
+    close[0].style.display = 'block';
+    menu[0].style.display = 'none';
   } else {
-  buttons[0].style.display = 'none'
-  main[0].style.filter = 'none'
-  close[0].style.display = 'none'
-  menu[0].style.display = 'block'
+    buttons[0].style.display = 'none';
+    main[0].style.filter = 'none';
+    close[0].style.display = 'none';
+    menu[0].style.display = 'block';
   }
-  } 
+};
+
+handleClick();
