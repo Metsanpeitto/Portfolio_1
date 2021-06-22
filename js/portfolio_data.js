415cc8868c2298b467f2d972c970560384290fec
Metsanpeitto <abraham@waldenberginc.com>
2021-06-23 00:19:29 +0200

    Fix linters

---
+++ js/portfolio_data.js
@@ -0,0 +1,40 @@
+const portfolio = [
+  {
+    textTitle: 'Tonic',
+    image: 'box__img image-1',
+    bread: ['CANOPY', 'Back End Dev', '2015'],
+    parraf:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    bubbles: ['html', 'css', 'javaScript'],
+    buttonId: 'portfolio-1',
+  },
+  {
+    textTitle: 'Multi-Post Stories',
+    image: 'box__img image-2',
+    bread: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    parraf:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    bubbles: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    buttonId: 'portfolio-2',
+  },
+  {
+    textTitle: 'Facebook 360',
+    image: 'box__img image-3',
+    bread: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    parraf:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    bubbles: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    buttonId: 'portfolio-3',
+  },
+  {
+    textTitle: 'Uber Navigation',
+    image: 'box__img image-4',
+    bread: ['Uber', 'Lead Developer', '2018'],
+    parraf:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    bubbles: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    buttonId: 'portfolio-4',
+  },
+];
+
+export default portfolio;
