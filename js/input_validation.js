2d72284369d250e11dc0873f8cffe361b034795c
Metsanpeitto <abraham@waldenberginc.com>
2021-06-24 14:36:12 +0200

    Fix linters

--- js/input_validation.js
+++ js/input_validation.js
@@ -3,14 +3,14 @@
  *   It checks that only lowercase is being typed into
  *   the email input of the contact form.
  */
- const contactForm = document.getElementById('form');
- const email = document.getElementById('email-address');
- const alertMessage = document.createElement('span');
- alertMessage.className = 'alert';
- alertMessage.textContent = 'The email can be only in lowercase';
- contactForm.addEventListener('submit', (e) => {
-   if (email.value.toLowerCase() !== email.value) {
-     e.preventDefault();
-     email.parentNode.insertBefore(alertMessage, email.nextSibling);
-   }
- });
+const contactForm = document.getElementById('form');
+const email = document.getElementById('email-address');
+const alertMessage = document.createElement('span');
+alertMessage.className = 'alert';
+alertMessage.textContent = 'The email can be only in lowercase';
+contactForm.addEventListener('submit', (e) => {
+  if (email.value.toLowerCase() !== email.value) {
+    e.preventDefault();
+    email.parentNode.insertBefore(alertMessage, email.nextSibling);
+  }
+});
