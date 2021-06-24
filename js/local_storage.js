d012e5756593d95e677ab62d750e79e8ebcc4849
ntzwilly <ntzwilly@gmail.com>
2021-06-24 18:00:26 +0200

    add storageAvailable function

--- js/local_storage.js
+++ js/local_storage.js
@@ -10,6 +10,35 @@
 // are different from the ones stored in localStorage.
 const data = { name: null, email: null, message: null }
 
+// This function will check that the browser has a localStorage system and if it exists
+// it would use it
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException
+      // everything except Firefox
+      && (e.code === 22
+        // Firefox
+        || e.code === 1014
+        // test name field too, because code might not be present
+        // everything except Firefox
+        || e.name === 'QuotaExceededError'
+        // Firefox
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      // acknowledge QuotaExceededError only if there's something already stored
+      && storage
+      && storage.length !== 0
+    );
+  }
+}
+
 // Every change that occurs in the fomr input fields will be parsed into the variabl 'data'
 function parseIt(v, index) {
   let name
