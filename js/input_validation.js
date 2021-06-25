1f7dc1a5d0d32eccd08a783decca9f500709bd6d
Metsanpeitto <abraham@waldenberginc.com>
2021-06-25 07:12:46 +0200

    Fix conflicts with git

--- js/input_validation.js
+++ js/input_validation.js
@@ -6,25 +6,24 @@
 // If the form is correctly filled the function storeIt is called
 // to proceed with the storage of the form parameters
 
-import storeIt from './local_storage.js';
+import storeIt from './local_storage.js'
 
-
-const contactForm = document.getElementById('form');
-const email = document.getElementById('email-address');
-const alertMessage = document.createElement('span');
-alertMessage.className = 'alert';
-alertMessage.textContent = 'The email can be only in lowercase';
+const contactForm = document.getElementById('form')
+const email = document.getElementById('email-address')
+const alertMessage = document.createElement('span')
+alertMessage.className = 'alert'
+alertMessage.textContent = 'The email can be only in lowercase'
 contactForm.addEventListener('submit', (e) => {
   if (email.value.toLowerCase() !== email.value) {
-    e.preventDefault();
-    email.parentNode.insertBefore(alertMessage, email.nextSibling);
+    e.preventDefault()
+    email.parentNode.insertBefore(alertMessage, email.nextSibling)
   } else {
-    storeIt();
+    storeIt()
     document
       .getElementById('message')
       .parentNode.insertBefore(
         alertMessage,
-        document.getElementById('message').nextSibling,
-      );
+        document.getElementById('message').nextSibling
+      )
   }
-});
+})
